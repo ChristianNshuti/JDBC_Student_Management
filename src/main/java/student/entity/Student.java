88cfd82a71ccc25c2d7629d858039635c95cef2d
christiannshuti <chrisnshuti943@gmail.com>
2026-03-04 21:42:29 +0200

    Implemented main.java and made few changes in Student entity

--- src/main/java/student/entity/Student.java
+++ src/main/java/student/entity/Student.java
@@ -18,7 +18,7 @@ public class Student {
     }
 
     public Long getId() { return id; }
-    public void setId(long id) { this.id = id }
+    public void setId(long id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
